93b67da81ec11656f34c6eb934a6482143e53227
anisa <anisahime@gmail.com>
2025-02-18 03:47:28 +1000

    переработка структуры веб-страниц

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
-import os
+
 
 hostName = "localhost"
 serverPort = 8080
 
 
 class MyServer(BaseHTTPRequestHandler):
-    """Класс для запуска и работы сервера с веб-приложением"""
+    """Класс для обработки входящих запросов от пользователей"""
     def do_GET(self):
         if self.path == "/":
             self.send_response(200)
@@ -14,14 +14,9 @@ class MyServer(BaseHTTPRequestHandler):
             self.end_headers()
 
             with open("contacts.html", "r", encoding="utf-8") as file:
-                html_content = file.read()
-            self.wfile.write(bytes(html_content, "utf-8"))
+                page_content = file.read()
 
-        else:
-            self.send_response(404)
-            self.send_header("Content-type", "text/html")
-            self.end_headers()
-            self.wfile.write(bytes("<h1>404 Not Found</h1>", "utf-8"))
+            self.wfile.write(bytes(page_content, "utf-8"))
 
 
 if __name__ == "__main__":
