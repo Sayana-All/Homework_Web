212152cc5a590ee5e3834a227ccd7324ab9591ee
anisa <anisahime@gmail.com>
2025-02-17 23:02:23 +1000

    переработка структуры веб-страниц

--- main.py
+++ main.py
@@ -1,70 +1,27 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
-from urllib.parse import urlparse, parse_qs
+import os
 
 hostName = "localhost"
 serverPort = 8080
 
 
 class MyServer(BaseHTTPRequestHandler):
-
-    def __get_index(self):
-        return f"""
-        <html lang="ru">
-        <head>
-            <meta charset="UTF-8">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <title>Меню сайта</title>
-            <link href="/static/css/bootstrap.min.css" rel="stylesheet">
-        </head>
-        <body>
-            <div class="container text-center">
-                <div class="row mt-5">
-                <div class="col-3 p-3 mb-2 bg-dark text-white w-100">
-                    <nav class="navbar bg-body-tertiary">
-                        <div class="container-fluid">
-                            <a class="navbar-brand" href="#">
-                                <img src="/docs/5.3/assets/brand/bootstrap-logo.svg" alt="Logo" width="30" height="24" class="d-inline-block align-text-top">
-                                    Меню
-                            </a>
-                        </div>
-                    </nav>
-                    <div class="d-flex align-items-start mt-3">
-                        <div class="nav flex-column nav-pills me-3" id="v-pills-tab" role="tablist" aria-orientation="vertical">
-                            <button class="nav-link active" id="v-pills-home-tab" data-bs-toggle="pill" data-bs-target="#v-pills-home" type="button" role="tab" aria-controls="v-pills-home" aria-selected="true">Главная</button>
-                            <button class="nav-link" id="v-pills-profile-tab" data-bs-toggle="pill" data-bs-target="#v-pills-profile" type="button" role="tab" aria-controls="v-pills-profile" aria-selected="false">Категории</button>
-                            <button class="nav-link" id="v-pills-messages-tab" data-bs-toggle="pill" data-bs-target="#v-pills-messages" type="button" role="tab" aria-controls="v-pills-messages" aria-selected="false">Заказы</button>
-                            <button class="nav-link" id="v-pills-settings-tab" data-bs-toggle="pill" data-bs-target="#v-pills-settings" type="button" role="tab" aria-controls="v-pills-settings" aria-selected="false">Контакты</button>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-8">
-                    <div class="tab-content" id="v-pills-tabContent">
-                        <div class="tab-pane fade show active" id="v-pills-home" role="tabpanel" aria-labelledby="v-pills-home-tab" tabindex="0">
-                            <h2>Главная</h2>
-                        </div>
-                        <div class="tab-pane fade" id="v-pills-profile" role="tabpanel" aria-labelledby="v-pills-profile-tab" tabindex="1">
-                            <h2>Категории</h2>
-                        </div>
-                        <div class="tab-pane fade" id="v-pills-messages" role="tabpanel" aria-labelledby="v-pills-messages-tab" tabindex="2">
-                            <h2>Каталог</h2>
-                        </div>
-                        <div class="tab-pane fade" id="v-pills-settings" role="tabpanel" aria-labelledby="v-pills-settings-tab" tabindex="3">
-                            <h2>Контакты</h2>
-                        </div>
-                    </div>
-                </div>
-                </div>
-            </div>
-        </body>
-        </html>
-        """
-
+    """Класс для запуска и работы сервера с веб-приложением"""
     def do_GET(self):
-        page_content = self.__get_index()
-        self.send_response(200)
-        self.send_header("Content-type", "text/html")
-        self.end_headers()
-        self.wfile.write(bytes(page_content, "utf-8"))
+        if self.path == "/":
+            self.send_response(200)
+            self.send_header("Content-type", "text/html")
+            self.end_headers()
+
+            with open("contacts.html", "r", encoding="utf-8") as file:
+                html_content = file.read()
+            self.wfile.write(bytes(html_content, "utf-8"))
+
+        else:
+            self.send_response(404)
+            self.send_header("Content-type", "text/html")
+            self.end_headers()
+            self.wfile.write(bytes("<h1>404 Not Found</h1>", "utf-8"))
 
 
 if __name__ == "__main__":
